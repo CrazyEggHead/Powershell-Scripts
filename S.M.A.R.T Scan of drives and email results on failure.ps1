9595ecd069071cfca07f1f2e43e2c1d1c6a3af9a
CrazyEggHead <96538006+CrazyEggHead@users.noreply.github.com>
2021-12-23 14:42:21 -0500

    Add files via upload

---
+++ S.M.A.R.T Scan of drives and email results on failure.ps1
@@ -0,0 +1,40 @@
+# Source: https://www.smartmontools.org/wiki/NVMe_Support
+
+$results = $null
+\\UNC-Path\PDQ_Repo_2\SmartCTL\smartctl --scan | Out-File disks.csv
+$disktest = get-content .\disks.csv
+
+foreach ($item in $disktest){
+$results += \\UNC-Path\PDQ_Repo_2\SmartCTL\smartctl -a (($item.Split(" "))[0]) #
+}
+
+$results | Select-string 'Product','Device Model','Health Status','overall-health'
+
+###########################################################################
+# This section forces email to mail-server to go over VPN connection and out to cloud (emailing from home public IP addresses is not possible)
+if ($results -like '*failure*') { 
+
+    Add-Type -AssemblyName PresentationFramework
+
+    #resolve dns servers for outlook
+    $outlookdns = Resolve-DnsName -name server.mail.protection.outlook.com 
+    #Add route for outlook to email from company IP over VPN (useful for split tunnel VPN)
+    foreach ($value in $outlookdns)
+    {
+    route ADD @($value.IPAddress) MASK 255.255.255.255 192.168.75.1
+    }
+    start-sleep (3)
+###########################################################################
+# Variable creation and email IT Group
+    $results | Out-File .\results.txt # Need to export to txt for line breaks to be included in email
+    $resultshtml = Get-Content .\results.txt | Out-String
+    $myhostname = [System.Net.Dns]::GetHostName()  #retrieve hostname of machine scan was run on
+    $MailFrom = 'system@domain.com'
+    $MailTo = 'it@domain.com'
+    $MailServer = 'server.mail.protection.outlook.com'
+    $MailSubject = "Hard drive failure on host $myhostname"
+    $MailBody = "Computer with Hard Drive issues: `n $myhostname `n `n ======================================================================== `n ***Results of Scan*** `n $resultshtml"
+    remove-item results.txt -Force # File Cleanup
+    Send-MailMessage -From $MailFrom -To $MailTo -Subject $MailSubject -Body ($MailBody | Out-String) -SmtpServer $MailServer
+}
+remove-item disks.csv -Force # File cleanup
